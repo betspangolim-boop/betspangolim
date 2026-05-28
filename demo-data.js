0094446b6f26dc2853d260aa86884b3ef7c7fa6f
betspangolim-boop <betspangolim@gmail.com>
2026-05-28 11:26:10 -0300

    Add files via upload

---
+++ demo-data.js
@@ -0,0 +1,16 @@
+export const demoData = {
+  jogos: [
+    { id:'demo-1', fontes:['Demo','SofaScore','AiScore','FullTBet'], cat:'nacional', camp:'Premier League', pais:'Inglaterra', status:'future', data:'2026-05-30 11:00', casa:'Arsenal', fora:'Chelsea', placar:'-', min:null, xg:[1.72,1.18], stats:{pos:[56,44],fin:[14,9],gol:[5,3],esc:[6,4],over25:58,btts:61,casa:49,empate:27,fora:24}, eventos:[] },
+    { id:'demo-2', fontes:['Demo','SofaScore','AiScore','FullTBet'], cat:'nacional', camp:'La Liga', pais:'Espanha', status:'live', data:'2026-05-28 16:30', casa:'Barcelona', fora:'Sevilla', placar:'1-0', min:38, xg:[1.12,.34], stats:{pos:[63,37],fin:[9,3],gol:[4,1],esc:[5,1],over25:64,btts:48,casa:66,empate:20,fora:14}, eventos:[['12','Gol','Barcelona','Atacante 9'],['28','Cartão amarelo','Sevilla','Volante 5']] },
+    { id:'demo-3', fontes:['Demo','SofaScore','AiScore','FullTBet'], cat:'nacional', camp:'Brasileirão Série A', pais:'Brasil', status:'future', data:'2026-05-31 18:00', casa:'Flamengo', fora:'Palmeiras', placar:'-', min:null, xg:[1.39,1.26], stats:{pos:[52,48],fin:[12,11],gol:[4,4],esc:[5,5],over25:47,btts:55,casa:39,empate:30,fora:31}, eventos:[] },
+    { id:'demo-4', fontes:['Demo','SofaScore','AiScore'], cat:'copa-internacional', camp:'UEFA Champions League', pais:'Europa', status:'past', data:'2026-05-25 16:00', casa:'Real Madrid', fora:'Bayern', placar:'2-1', min:90, xg:[1.66,1.44], stats:{pos:[51,49],fin:[15,13],gol:[6,5],esc:[7,6],over25:59,btts:63,casa:44,empate:27,fora:29}, eventos:[['21','Gol','Bayern','Centroavante'],['54','Gol','Real Madrid','Meia'],['83','Gol','Real Madrid','Ponta']] },
+    { id:'demo-5', fontes:['Demo','SofaScore','FullTBet'], cat:'copa-nacional', camp:'Copa do Brasil', pais:'Brasil', status:'future', data:'2026-06-02 21:30', casa:'Corinthians', fora:'Santos', placar:'-', min:null, xg:[1.31,1.05], stats:{pos:[54,46],fin:[10,8],gol:[4,3],esc:[6,3],over25:43,btts:49,casa:42,empate:31,fora:27}, eventos:[] },
+    { id:'demo-6', fontes:['Demo','AiScore'], cat:'internacional', camp:'FIFA Club World Cup', pais:'Mundo', status:'future', data:'2026-06-05 15:00', casa:'Manchester City', fora:'Al Ahly', placar:'-', min:null, xg:[2.05,.82], stats:{pos:[64,36],fin:[16,6],gol:[7,2],esc:[8,2],over25:62,btts:38,casa:71,empate:18,fora:11}, eventos:[] }
+  ],
+  times: [],
+  odds: [
+    { jogo:'demo-1', mercado:'Match Odds', back:{casa:2.05,empate:3.45,fora:3.80}, lay:{casa:2.12,empate:3.60,fora:3.95} },
+    { jogo:'demo-2', mercado:'Match Odds', back:{casa:1.54,empate:4.25,fora:6.20}, lay:{casa:1.57,empate:4.40,fora:6.60} },
+    { jogo:'demo-3', mercado:'Match Odds', back:{casa:2.42,empate:3.10,fora:3.05}, lay:{casa:2.50,empate:3.25,fora:3.18} }
+  ]
+};
